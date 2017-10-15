fef6656f87c922ee80e5f27b582652d302382919
maciejstanek <makaron28@gmail.com>
2017-10-15 23:27:32 +0200

    Ready to implement raw encryption.

--- 04/tea.c
+++ 04/tea.c
@@ -5,53 +5,103 @@
 enum {ENCODE, DECODE} algorithmDirection;
 
 void printUsage() {
-  printf("\n");
-  printf("Usage: ./a.out <encode|decode> <key>\n");
-  printf("\n");
-  printf("       Pass plain text or encoded text\n");
-  printf("       via 'stdin'. The program outputs\n");
-  printf("       to 'stdout'. Ciphertext is in hex\n");
-  printf("       numbers - not raw binary.\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "Usage: ./a.out <encrypt|decrypt> <key>\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "       Pass plain text or encrypted text\n");
+  fprintf(stderr, "       via 'stdin'. The program outputs\n");
+  fprintf(stderr, "       to 'stdout'. Ciphertext is in hex\n");
+  fprintf(stderr, "       numbers - not raw binary.\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "       The key is required to be exactly\n");
+  fprintf(stderr, "       %lu characters long. This means that\n", 4 * sizeof(int));
+  fprintf(stderr, "       the key has %lu bits. All the characters\n", 4 * sizeof(int) * 8);
+  fprintf(stderr, "       exceeding this length are trimmed.\n");
 }
 
 void printBanner() {
-  printf("TEA - a Tiny Encryption Algorithm\n");
+  fprintf(stderr, "+-------------------------------------+\n");
+  fprintf(stderr, "|                                     |\n");
+  fprintf(stderr, "|   TEA - Tiny Encryption Algorithm   |\n");
+  fprintf(stderr, "|                                     |\n");
+  fprintf(stderr, "+-------------------------------------+\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "Note: https://en.wikipedia.org/wiki/Tiny_Encryption_Algorithm\n");
 }
 
-char *readPlainText() {
-  int stringLength = 1;
-  char *string = malloc(sizeof(char));
+void encryptTwoValues(unsigned int *value1, unsigned int *value2, unsigned int* key) {
+  // This is where raw encryption takes place.
+  // TODO
+}
+
+void encryptStdStreamOnTheFly(unsigned int *key) {
   char inputChar;
-  while(scanf("%c", &inputChar) != EOF) {
-    // TODO: Change this uneffective way of allocating memory
-    string[stringLength - 1] = inputChar;
-    string = realloc(string, ++stringLength * sizeof(char));
+  unsigned char inputBuffer[2 * sizeof(int)];
+  enum {true = 1, false = 0} endOfFile = false;
+  while(!endOfFile) {
+    int i = 0;
+    for(; i < 2 * sizeof(int); i++) {
+      if(scanf("%c", &inputBuffer[i]) == EOF) {
+        endOfFile = true;
+        break;
+      }
+    }
+    if(endOfFile) {
+      // Pad with zeros to a full chunk
+      for(; i < 2 * sizeof(int); i++) {
+        inputBuffer[i] = 0;
+      }
+    }
+    unsigned int value[2];
+    for(int i = 0; i < 2; i++) {
+      value[i] = 0;
+      for(int j = 0; j < sizeof(int); j++) {
+        value[i] |= inputBuffer[i * sizeof(int) + j] << 8 * (sizeof(int) - 1 - j);
+      }
+    }
+    fprintf(stderr, "(0x%08x, 0x%08x) -> ", value[0], value[1]);
+    encryptTwoValues(&value[0], &value[1], key);
+    fprintf(stderr, "(0x%08x, 0x%08x)\n", value[0], value[1]);
   }
-  string[stringLength] = '\0';
-  return string;
+}
+
+unsigned int *parseKey(char *rawKey) {
+  unsigned int *key = calloc(4, sizeof(int));
+  fprintf(stderr, "Note: Interpreting the key as\n");
+  fprintf(stderr, "      0x");
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < sizeof(int); j++) {
+      key[i] |= ((unsigned int)rawKey[j]) << 8 * (sizeof(int) - 1 - j);
+    }
+    fprintf(stderr, "%08x", key[i]);
+  }
+  fprintf(stderr, "\n");
 }
 
 int parseArgs(int argc, char **argv) {
   if(argc == 1) {
     printUsage();
-    return 0;
+    return 1;
   }
   if(argc != 3) {
-    printf("Wrong number of arguments.\n");
+    fprintf(stderr, "Error: Wrong number of arguments.\n");
     printUsage();
     return 1;
   }
-  if(strcmp(argv[1], "encode") == 0) {
+  if(strcmp(argv[1], "encrypt") == 0) {
     algorithmDirection = ENCODE;
-  } else if(strcmp(argv[1], "decode") == 0) {
+  } else if(strcmp(argv[1], "decrypt") == 0) {
     algorithmDirection = DECODE;
   } else {
-    printf("Wrong algorithm direction.\n");
-    printf("Allowed are 'encode' and 'decode'.\n");
+    fprintf(stderr, "Error: Wrong algorithm direction.\n");
     printUsage();
     return 1;
   }
-  // TODO
+  if(strlen(argv[2]) < 4 * sizeof(int)) {
+    fprintf(stderr, "Error: Wrong key size.\n");
+    printUsage();
+    return 1;
+  }
   return 0;
 }
 
@@ -60,13 +110,12 @@ int main(int argc, char **argv) {
   if(parseArgs(argc, argv)) {
     return 1;
   }
+  unsigned int *key = parseKey(argv[2]);
   if(algorithmDirection == ENCODE) {
-    printf("ENCODING\n");
-    char *plainText = readPlainText();
-    printf("(%s)\n", plainText);
-    printf("(%d)\n", (unsigned)strlen(plainText));
+    fprintf(stderr, "Note: Encrypting...\n");
+    encryptStdStreamOnTheFly(key);
   } else {
-    printf("DECODING\n");
+    fprintf(stderr, "Note: Decrypting...\n");
   }
   return 0;
 }
--- 04/test.sh
+++ 04/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -n 1234567890 | ./a.out encode 123
+echo -n 1234567890 | ./a.out encrypt a1b2c3d4e5f6g7h8
