b14bbac99beb355dc92478a6fd54ac9d3888604b
maciejstanek <makaron28@gmail.com>
2018-03-18 13:45:25 +0100

    Upgraded a fizzbuzzer by adding erro handling.

--- 05/fizzbuzz.tcl
+++ 05/fizzbuzz.tcl
@@ -1,6 +1,22 @@
 #!/usr/bin/tclsh
 
-for {set i 1} {$i < 100} {incr i} {
+set default 100
+if {[llength $argv] < 1} {
+  puts stderr "Argument not given, assuming $default."
+  set limit $default
+} else {
+  set limit [lindex $argv 0]
+}
+if {![string is integer -strict $limit]} {
+  puts stderr "Argument is not an integer, assuming $default."
+  set limit $default
+}
+if {$limit < 1} {
+  puts stderr "Argument has to be 1 or more, assuming $default."
+  set limit $default
+}
+
+for {set i 1} {$i < $limit + 1} {incr i} {
   set output {}
   if {$i % 3 == 0} {
     append output Fizz
